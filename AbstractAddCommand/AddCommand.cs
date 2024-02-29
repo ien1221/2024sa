07c2ee99a66b020f2de352550cf98410b3e3d389
ien1221 <bb11225544@gmail.com>
2024-02-29 23:24:16 +0800

    Modify .sln

---
+++ AbstractAddCommand/AddCommand.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Tasks.Command
+{
+    public interface AddCommand
+    {
+        public void Execute() { }
+    }
+}
---
+++ AbstractAddCommand/AddProject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasks.Command
+{
+    public class AddProject : AddCommand
+    {
+        public void Execute() { }
+    }
+}
